b00f9073bcad788349789643d066f2b39de9b425
anunyaOrange <anunyaOrange@proton.me>
2025-06-19 13:48:57 +0700

    update 2

--- myApp.js
+++ myApp.js
@@ -4,7 +4,8 @@ const helmet = require('helmet');
 
 app.use(helmet.hidePoweredBy());
 app.use(helmet.frameguard({ action: 'deny' }));
-
+app.use(helmet.xssFilter());
+app.use(helmet.noSniff());
 
 app.use('/public', express.static('public'));
 
